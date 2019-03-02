52e686014e96e83cd151712387fee6de80a84129
skylarb <skylar_baley@yahoo.com>
2019-03-01 18:07:51 -0600

    Finished Home Screen Functionality.  Buttons display Error Message letting the user know the functionality is unavailable.  Log Out returns user to Login screen.

--- app/src/main/java/edu/uark/uarkregisterapp/HomeScreen.java
+++ app/src/main/java/edu/uark/uarkregisterapp/HomeScreen.java
@@ -1,19 +1,41 @@
 package edu.uark.uarkregisterapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import org.apache.commons.lang3.StringUtils;
+
 import edu.uark.uarkregisterapp.models.transition.EmployeeTransition;
 
 public class HomeScreen extends AppCompatActivity {
+
+    String errMessage = "Functionality Not Available";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
     }
+
+    public void startTransactionOnClick(View view) {
+        new AlertDialog.Builder(this).
+                setMessage(errMessage).
+                setPositiveButton(
+                        R.string.button_dismiss,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }
+                ).
+                create().
+                show();
+    }
+
 
     public void createNewEmployeeButton(View view) {
         Intent intent = new Intent(getApplicationContext(), CreateEmployeeActivity.class);
@@ -23,5 +45,39 @@ public class HomeScreen extends AppCompatActivity {
                 new EmployeeTransition()
         );
         this.startActivity(intent);
+    }
+
+    public void salesReportProductOnClick(View view) {
+        new AlertDialog.Builder(this).
+                setMessage(errMessage).
+                setPositiveButton(
+                        R.string.button_dismiss,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }
+                ).
+                create().
+                show();
+    }
+
+    public void salesReportCashierOnClick(View view) {
+        new AlertDialog.Builder(this).
+                setMessage(errMessage).
+                setPositiveButton(
+                        R.string.button_dismiss,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }
+                ).
+                create().
+                show();
+    }
+
+    public void logOutOnClick(View view) {
+        this.startActivity(new Intent(getApplicationContext(), LoginScreenActivity.class));
     }
 }
