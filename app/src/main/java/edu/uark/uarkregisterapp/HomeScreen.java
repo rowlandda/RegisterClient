2519c6b4346ddd4c246ae4d7fa4e5347f20bc9bc
skylarb <skylar_baley@yahoo.com>
2019-04-08 20:30:33 -0500

    Toolbar Implemented
    Toolbar has been implemented on the HomeScreen.  Still testing more to add additional icons.  Trying to determine modularity, create one menu and use repeatedly.  As it looks right now, there is still a substantial codeblock needed to have clickable items in menu.

--- app/src/main/java/edu/uark/uarkregisterapp/HomeScreen.java
+++ app/src/main/java/edu/uark/uarkregisterapp/HomeScreen.java
@@ -5,7 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import edu.uark.uarkregisterapp.models.transition.EmployeeTransition;
 
@@ -18,8 +23,43 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         this.currentEmployeeTransition = this.getIntent().getParcelableExtra("intent_create_employee");
     }
+
+    //===========================================================
+    //Adds Menu at the top
+    //===========================================================
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Fruit selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Protein selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Gear selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(this, "Gift Card selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //End Menu
+    //===========================================================
 
     public void startTransactionOnClick(View view) {
         Intent intent = new Intent(getApplicationContext(), ProductsListingActivity.class);
